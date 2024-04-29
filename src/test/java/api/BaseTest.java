c3bc5236b963aada4852964ec56e1cab11234487
rameez <rameezf@globalkinetic.com>
2024-04-29 09:06:28 +0200

    Delete

--- src/test/java/api/BaseTest.java
+++ src/test/java/api/BaseTest.java
@@ -1,18 +1,23 @@
 package api;
 
 import io.restassured.RestAssured;
+import org.slf4j.Logger;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 import static api.common.TestData.DATA_PATH;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public abstract class BaseTest {
 
     public static Properties properties;
     public static CustomConfig customConfig;
+    private static final Logger LOG = getLogger(lookup().lookupClass());
 
     @BeforeMethod
     public void setup() throws IOException {
@@ -24,6 +29,8 @@ public abstract class BaseTest {
         properties = new Properties();
         try (FileInputStream file = new FileInputStream(DATA_PATH)) {
             properties.load(file);
+        } catch (FileNotFoundException fileNotFound) {
+            LOG.info(String.format("Properties file not found %s", fileNotFound));
         }
         return properties;
     }
